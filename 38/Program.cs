cd136798c15565f8ee920e51fa655cf727901c90
Andrey <Frolovandr97@mail.ru>
2023-03-13 13:13:02 +0700

    Разница между мin и мах

--- 38/Program.cs
+++ 38/Program.cs
@@ -2,3 +2,43 @@
 
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Получен массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int n = 0; n < numbers.Length; n++)
+{
+    if (numbers[n] > max)
+    {
+        max = numbers[n];
+    }
+    if (numbers[n] < min)
+    {
+        min = numbers[n];
+    }
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 10;
+    }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
