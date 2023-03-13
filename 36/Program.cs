7161e4a3b9dd6b73b816ca7c649050ea807cd123
Andrey <Frolovandr97@mail.ru>
2023-03-13 12:59:44 +0700

    Сумма нечетных элементов массива

--- 36/Program.cs
+++ 36/Program.cs
@@ -5,3 +5,33 @@
 // [-4, -6, 89, 6] -> 0
 
 
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Получен массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int n = 0; n < numbers.Length; n += 2)
+    sum = sum + numbers[n];
+
+Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-99, 99);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
